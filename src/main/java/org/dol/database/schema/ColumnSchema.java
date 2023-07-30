65b78bc2e8d55d02b58df5bcbe47a67696a48d97
dolphin <1014840594@qq.com>
2023-07-30 10:43:48 +0800

    change like column size to 129

--- src/main/java/org/dol/database/schema/ColumnSchema.java
+++ src/main/java/org/dol/database/schema/ColumnSchema.java
@@ -861,7 +861,7 @@ public class ColumnSchema {
     public boolean isLikeWhere() {
 
         return isCharOrVarcharColumn()
-                && columnSize < 128
+                && columnSize < 129
                 && !(isUpdateTimeColumn()
                 || isCreateTimeColumn()
                 || isDateColumn()
@@ -870,7 +870,7 @@ public class ColumnSchema {
 
     public boolean isKeywordColumn() {
         return isCharOrVarcharColumn()
-                && columnSize < 128
+                && columnSize < 129
                 && !(isUpdateTimeColumn()
                 || isCreateTimeColumn()
                 || isDateColumn()
